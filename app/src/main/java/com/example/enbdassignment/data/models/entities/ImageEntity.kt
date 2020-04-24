1b1280cc5048ec539bece473f372970a2f25d5ed
Faris Yousuf <faris@farabi.ae>
2020-04-24 12:08:47 +0400

    Serialized names added for response model object

--- app/src/main/java/com/example/enbdassignment/data/models/entities/ImageEntity.kt
+++ app/src/main/java/com/example/enbdassignment/data/models/entities/ImageEntity.kt
@@ -3,19 +3,21 @@ package com.example.enbdassignment.data.models.entities
 import android.os.Parcelable
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.example.enbdassignment.data.*
+import com.google.gson.annotations.SerializedName
 import kotlinx.android.parcel.Parcelize
 
 @Parcelize
 @Entity(tableName = "images")
 data class ImageEntity(
     @PrimaryKey
-    val id: Int,
-    val comments: Int? = null,
-    val previewURL: String? = null,
-    val webformatURL: String? = null,
-    val largeImageURL: String? = null,
-    val likes: Int? = null,
-    val tags: String? = null,
-    val type: String? = null,
-    var searchWord: String? = null
+    @SerializedName(ID) val id: Int,
+    @SerializedName(COMMENTS) val comments: Int? = null,
+    @SerializedName(PREVIEW_URL) val previewURL: String? = null,
+    @SerializedName(WEB_FORMAT_URL) val webformatURL: String? = null,
+    @SerializedName(LARGE_IMAGE_URL) val largeImageURL: String? = null,
+    @SerializedName(LIKES) val likes: Int? = null,
+    @SerializedName(TAGS) val tags: String? = null,
+    @SerializedName(TYPE) val type: String? = null,
+    @SerializedName(SEARCH_WORD) var searchWord: String? = null
 ) : Parcelable
